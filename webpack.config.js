ae167da9c7fb935091285a778e3e08021e671203
alptarik <alptarik.salman@gmail.com>
2024-11-22 17:45:58 +0300

    evettts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: process.env.NODE_ENV || 'development',
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
